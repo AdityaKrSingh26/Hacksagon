26b8d6575e255efd3807d70085e782f3311f39a9
Dhrushit Jain <bcs_2023019@iiitm.ac.in>
2025-02-05 17:38:14 +0530

    ieee india council

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -4,7 +4,7 @@ const Partners = () => {
   return ( 
     <div className="flex flex-col items-center ">
       <h1 className="text-6xl font-semibold mt-4 mb-4">Technical Partners</h1>
-      <div className="flex gap-12 w-3/4 mx-4 my-4 p-4 flex-wrap justify-center rounded-lg bg-white/15">
+      <div className="flex gap-12 w-3/4 mx-4 my-4 p-4 flex-wrap justify-center items-center rounded-lg bg-white/15">
         {TechnicalPartners.map((partner, index) => (
           <div key={index}>
             <img src={partner.img} className="w-96" alt="" />
