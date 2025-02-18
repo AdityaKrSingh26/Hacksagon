0e49b4f76690fc2638da0c267e16aff6a83f5121
Prasun Baranwal <prasubaran@gmail.com>
2025-02-18 14:36:21 +0530

    pdf link updated

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -22,7 +22,7 @@ export function PdfViewer() {
         </div>
         <div style={{ height: "75vh", width: "75%" }}>   
           <iframe
-            src="https://jmp.sh/gps1kReO"
+            src="https://jmp.sh/dN2PcUqh"
             title="PDF Viewer"
             width="100%"
             height="100%"
